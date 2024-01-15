85f2afaddaef30ed4ddda4bfa0b76dada2dcf25b
SRESamuel <samuel.est10@hotmail.com>
2024-01-14 19:28:07 -0500

    Finished overall layout and styling, need to input proper project and add text as well as do bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const navBar = document.querySelector('[data-navBar]')
 const navTogglerElements = document.querySelectorAll('[data-nav-toggler]')
 const overlay = document.querySelector('[data-overlay]')
-
-
+const themeBtn = document.querySelector('#theme-btn')
+const originalBtn = document.querySelector('#originalTheme-btn')
+const body = document.querySelector('body')
 
 function handleElementsOnClick(elements, eventType, callbackFunction) {
     for (let i = 0; i < elements.length; i++) {
@@ -17,4 +18,37 @@ function handleToggleNavBar () {
 }
 
 handleElementsOnClick(navTogglerElements, 'click', handleToggleNavBar)
+
+themeBtn.addEventListener('click', () => {
+    // Cycle through the colors without going back to original
+    let currentClass = body.className; // Using className
+    switch (currentClass) {
+        case 'black':
+            body.className = 'green';
+            break;
+        case 'green':
+            body.className = 'red';
+            break;
+        case 'red':
+            body.className = 'blue';
+            break;
+        case 'blue':
+            body.className = 'yellow';
+            break;
+        case 'yellow':
+            body.className = 'purple';
+            break;
+        case 'purple':
+            body.className = 'g';
+            break;
+        // If 'g' or any other class, it will go back to 'black'
+        default:
+            body.className = 'black';
+    }
+});
+
+originalBtn.addEventListener('click', () => {
+    body.className = ''; // Clear any class setting it back to the default color
+});
+
 
